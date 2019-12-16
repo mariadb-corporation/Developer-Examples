410cd205a04ad5eaa74106e9cb848efcad1b400d
Rob Hedgpeth <rob.hedgpeth@gmail.com>
2019-12-16 15:14:44 -0800

    updates

--- Flights/src/db.js
+++ Flights/src/db.js
@@ -1,24 +1,11 @@
 var fs = require("fs")
 var mariadb = require('mariadb');
 
-/*
 const pool = mariadb.createPool({
-    host: 'localhost', 
-    user:'dba', 
-    port: 3307,
-    password: 'demo_password',
-    database: 'flights',
-    connectionLimit: 5
-});
-*/
-
-//const serverCert = [fs.readFileSync("skysql_root.pem", "utf8")];
-
-const pool = mariadb.createPool({
-  host: 'sky0001276.mdb0001390.db.skysql.net', 
-  user:'DB00001323', 
+  host: 'Host_Here', 
+  user:'Username_Here', 
   port: 5002,
-  password: "xqjvKYE6q7yE~J8X0N1,1i|6",
+  password: "Password_Here",
   database: 'flights',
   connectionLimit: 5
 });
