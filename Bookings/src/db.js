e6d183e7170d7102e827e049d098fa537a665f04
Rob Hedgpeth <rob.hedgpeth@gmail.com>
2020-01-30 13:34:13 -0800

    Update to db.js

--- Bookings/src/db.js
+++ Bookings/src/db.js
@@ -1,15 +1,15 @@
 var mariadb = require('mariadb');
 require('dotenv').config();
 
-var pool = mariadb.createPool({
-                host: process.env.DB_HOST, 
-                user: process.env.DB_USER, 
-                password: process.env.DB_PASS,
-                port: process.env.DB_PORT,
-                database: process.env.DB_NAME,
-                multipleStatements: true,
-                connectionLimit: 5
-            });
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    multipleStatements: true,
+    connectionLimit: 5
+});
 
 module.exports={
     getConnection: function(){
